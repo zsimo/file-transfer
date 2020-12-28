5e912c40894ecb12b1a8de364a67ad8c3cf9717f
Simone Sacchi <simone.sacchi@nubilaria.com>
2020-12-28 12:16:49 +0100

    minor tcp receiver

--- receivers/tcp.js
+++ receivers/tcp.js
@@ -12,23 +12,23 @@ fs.mkdirSync(filesDir, {recursive: true});
 var writer = fs.createWriteStream(path.resolve(filesDir, fileName));
 
 var server = net.createServer(function(socket) {
-    //socket.write('Echo server\r\n');
 
     var downloaded = 0;
     var start = Date.now();
     socket.on("data", function (chunk) {
         downloaded += chunk.length;
-        //var percent = Math.round((downloaded / totalLength) * 100) + "%";
         console.log(`receiving ${(downloaded / (1024 * 1024)).toFixed(2)}MB`);
     });
     writer.on("finish", function () {
         console.log("finish");
         console.log(`receiving ${fileName} terminated in ${Date.now() - start}ms`);
-
     });
     socket.pipe(writer);
 });
+server.on("connection", function (socket) {
+    console.log("client connected");
+});
 
-
-console.log("server listening on:", config.TCP_RECEIVER_PORT, '0.0.0.0');
-server.listen(config.TCP_RECEIVER_PORT, '0.0.0.0');
+server.listen(config.TCP_RECEIVER_PORT, '0.0.0.0', function () {
+    console.log("server listening on:", server.address());
+});
